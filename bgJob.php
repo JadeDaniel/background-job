59543e63c9f887f766c8484584bb0a0783dacec4
Jade Steffen <steffenjade@gmail.com>
2021-03-10 23:41:03 -0600

    Add arg handling, simple test, return PID.
    Based on further answers to same Stackoverflow question.

--- bgJob.php
+++ bgJob.php
@@ -5,50 +5,70 @@ namespace diversen;
 /**
  * Class that can execute a background job, and check if the
  * background job is still running
- * 
+ *
  * @package main
  */
+class bgJob
+{
 
-class bgJob {
-    
     /**
-     * Executes a background job using exec. A file with the content 
-     * of the background job's output will be created, and a file holding the 
-     * job's pid (process id). 
-     *   
+     * Executes a background job using exec. A file with the content
+     * of the background job's output will be created, and a file holding the
+     * job's pid (process id).
+     *
      * @param string $cmd the command
      * @param string $outputfile an output file where output of command will be written
-     * @param string $pidfile a file where the process number is written
+     * @param string $pidArray a file where the process number is written
      * @return boolean $res true on success and false on failure
      */
-    public function execute ($cmd, $outputfile, $pidfile) {
-        $res = exec(sprintf("%s > %s 2>&1 & echo $! > %s", $cmd, $outputfile, $pidfile));
-        if (!$res) {
-            $this->pid = trim(file_get_contents($pidfile));
-            return true;
+    public function execute(string $outputfile, $cmd, ...$args)
+    {
+        $cmd = escapeshellcmd($cmd);
+        $args = $this->prepareArgs($args);
+
+        $cmd .= " " . implode(" ", $args);
+        $res = exec(sprintf("%s > %s 2>&1 & echo $!", $cmd, $outputfile), $pid);
+        if ($pid) {
+            return $pid[0];
         }
         return false;
     }
-    
-    
-    public $pid = 0;
-    
+
     /**
-     * Check if a background process is running. 
-     * 
+     * Check if a background process is running.
+     *
      * @param int $pid the process id to check for
-     * @return boolean $res true if running or else false 
+     * @return boolean $res true if running or else false
      */
-    public function isRunning($pid) {
+    public function isRunning($pid)
+    {
         try {
             $result = shell_exec(sprintf("ps %d", $pid));
             if (count(preg_split("/\n/", $result)) > 2) {
                 return true;
             }
         } catch (Exception $e) {
-            
+
         }
         return false;
+    }
+
+    /**
+     * @param array $args
+     * @return array
+     */
+    public function prepareArgs(array $args): array
+    {
+        array_walk($args, function (&$v) {
+            if ($v === null || $v === '') {
+                $v = "''";
+            } elseif ($v === false || $v === "0" || $v === 0) {
+                $v = "0";
+            } else {
+                $v = escapeshellarg($v);
+            }
+        });
+        return $args;
     }
 }
 
---
+++ tests/bgJobTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace diversen;
+
+use PHPUnit\Framework\TestCase;
+
+class bgJobTest extends TestCase
+{
+    public function testPrepareArgs() {
+        $args = [
+            "",
+            null,
+            false,
+            0,
+            "actualData"
+        ];
+
+        $bgJob = new bgJob();
+        $prepared = $bgJob->prepareArgs($args);
+
+        foreach ($prepared as $argument) {
+            $this->assertNotSame(null, $argument);
+            $this->assertNotSame(false, $argument);
+            $this->assertNotSame(0, $argument);
+        }
+
+        $this->assertEquals("''", $prepared[0]);
+        $this->assertEquals("''", $prepared[1]);
+        $this->assertEquals("0", $prepared[2]);
+        $this->assertEquals("0", $prepared[3]);
+        $this->assertEquals("'actualData'", $prepared[4]);
+    }
+
+}
